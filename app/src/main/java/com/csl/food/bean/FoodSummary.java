802c1c98b64d24df10190018ffc6ec76332db17d
csl <123456>
2016-11-30 20:57:35 +0800

    Initial commit

---
+++ app/src/main/java/com/csl/food/bean/FoodSummary.java
@@ -0,0 +1,15 @@
+package com.csl.food.bean;
+
+/**
+ * @name: Food-master
+ * @class name：com.csl.food.bean
+ * @Fuction : 食品列表实体类
+ * @anthor :csl QQ:870588442
+ * @time :2016/11/30 19:32
+ * @change:
+ * @chang :time
+ * @class :describe
+ */
+public class FoodSummary {
+
+}
